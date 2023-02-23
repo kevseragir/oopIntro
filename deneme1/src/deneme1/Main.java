22ed474548a4f00e76af415ca0a91660a133cdaa
kevseragir <kevseragir68@gmail.com>
2023-02-23 12:59:51 +0300

    rasgele

---
+++ deneme1/src/deneme1/Main.java
@@ -0,0 +1,26 @@
+package deneme1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		 int max = 10;
+	        int min = 1;
+	        int range = max - min + 1;
+			
+		
+		
+	        for (int i = 0; i < 10; i++) {
+	            int rand = (int)(Math.random() * range) + min;
+	 
+	      
+	     System.out.println(rand);
+		
+		
+		
+		
+		
+	        }
+	}
+
+}
